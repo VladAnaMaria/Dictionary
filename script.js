e7d22c99ccc93fc936b47b21895d756cb95db3a2
VladAnaMaria <123776957+VladAnaMaria@users.noreply.github.com>
2023-02-12 23:55:23 +0200

    Update
    index and script

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 let item = 1, array = Array();
-function addWord() {
-    let word = document.getElementById('txt').value, wordLowerCase = word.toLowerCase();
+
+function addWord () {
+    let word = document.getElementById('txtAdd').value, wordLowerCase = word.toLowerCase();
     let alreadyExistsElement = array.some(element => element.includes(wordLowerCase));
     if (alreadyExistsElement == false) {
         if (wordLowerCase.indexOf(" ") >-1) {
-            document.getElementById('message').innerHTML ="Please write a word without spaces!";
-            document.getElementById('message').style="color:#ffc107"; 
+            document.getElementById('messageAdd').innerHTML ='Please write a word without spaces!';
+            document.getElementById('messageAdd').style='color:#ffc107'; 
         } else {
             array[item] = wordLowerCase;
-            document.getElementById('message').innerHTML ="Successfully added!";
-            document.getElementById('message').style="color:#198754";
+            document.getElementById('messageAdd').innerHTML ='Successfully added!';
+            document.getElementById('messageAdd').style='color:#198754';
             ++item;
         }
     } else {
         document.getElementById('message').innerHTML = "You have already saved this word!";
         document.getElementById('message').style="color:#dc3545";
     }
-    document.getElementById('txt').value = "";
+    document.getElementById('txtAdd').value = '';
+}
+
+function checkWord () {
+    let word = document.getElementById('txtCheck').value, wordLowerCase = word.toLowerCase();
+    let alreadyExistsElement = array.some(element => element.includes(wordLowerCase));
+    if (alreadyExistsElement == true) {
+        document.getElementById('messageCheck').innerHTML = 'The word is saved in the dictionary!';
+        document.getElementById('messageCheck').style='color:#198754';
+    } else {
+        document.getElementById('messageCheck').innerHTML = 'The word is not saved in the dictionary!';
+        document.getElementById('messageCheck').style='color:#dc3545';
+    }
+    document.getElementById('txtCheck').value = '';
 }
